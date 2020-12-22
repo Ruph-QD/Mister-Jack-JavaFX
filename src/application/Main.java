97b6a8843411eef2005d99d1bacc70200f6c55e9
maximeisep <72963776+maximeisep@users.noreply.github.com>
2020-12-22 23:00:29 +0100

    modifications du plateau

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		Parent root =FXMLLoader.load(getClass().getResource("Main.fxml"));	
-		primaryStage.setTitle("Yolo");
+		primaryStage.setTitle("Mr Jack Pocket");
 		
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toString());
